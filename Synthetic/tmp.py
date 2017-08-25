f0ad48e366541ebd39e04375d4b390400da30865
TramsWang <babyfish92@163.com>
2017-08-25 19:24:36 +0800

    Modify introduction of paper

--- Synthetic/tmp.py
+++ Synthetic/tmp.py
@@ -1,21 +1,44 @@
-from math import *
+import csv
+import Divergence
+import statistics
+import random
+import scipy.stats as stats
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import math
+import sys
 
-sa = 0.006571
-ua = 0.009587
-sb = 0.03078
-ub = 0.06759
-alpha = 0.1
 
-A = sb * sb - sa * sa
-B = -2 * (ua * sb * sb - ub * sa * sa)
-C = ua * ua * sb * sb - ub * ub * sa * sa - 2 * sa * sa * sb * sb * log(((1 - alpha) * sb) / (alpha * sa))
+reader = csv.reader(open("Correct/92.csv", 'r'))
+data = list(int(row[2]) for row in reader)
+reader = csv.reader(open("Centralized/92.csv", 'r'))
+data_cen = list(int(row[2]) for row in reader)
+step = 1403
 
-D = B * B - 4 * A * C
-print(A)
-print(B)
-print(C)
-print(D)
+dropped, dist = Divergence.histogram(data, step)
+dropped, dist_cen = Divergence.histogram(data_cen, step)
 
-print("Solution: ")
-print((-B + sqrt(D)) / (2 * A))
-print((-B - sqrt(D)) / (2 * A))
+figure = plt.figure(figsize=(1500/300, 900/300), dpi=300)
+acc = 0
+x = []
+ecdf = []
+for i in range(60):
+    if i in dist:
+        acc += dist[i]
+    x += [i, i+1]
+    ecdf += [acc, acc]
+plt.plot(x, ecdf, 'k', label="Correct")
+
+acc = 0
+x = []
+ecdf = []
+for i in range(60):
+    if i in dist_cen:
+        acc += dist_cen[i]
+    x += [i, i+1]
+    ecdf += [acc, acc]
+plt.plot(x, ecdf, 'r', label="Cheated")
+plt.legend()
+plt.grid()
+figure.savefig("ECDF.png")
